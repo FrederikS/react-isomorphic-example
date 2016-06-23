676f844c985d9f591ff0e6f835d9236895ece7cb
fsteffen <fsteffen@team.mobile.de>
2016-06-23 19:17:18 +0200

    expanded by default true

--- src/main/models/Topic.js
+++ src/main/models/Topic.js
@@ -4,13 +4,12 @@ export default class Topic {
     id;
     subject;
     posts = [];
-    @observable expanded = false;
+    @observable expanded = true;
 
     constructor(id, subject, posts) {
         this.id = id;
         this.subject = subject;
         this.posts = posts;
     }
-
 
 }
